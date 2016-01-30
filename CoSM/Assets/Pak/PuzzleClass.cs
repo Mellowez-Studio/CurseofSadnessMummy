2014a9a95e4f8c832c8783ea23cb557985b6f472
panumak <thopzabaying@gmail.com>
2016-01-30 23:05:37 +0700

    a

---
+++ CoSM/Assets/Pak/PuzzleClass.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleClass : MonoBehaviour
+{
+	PuzzleController.PuzzleColor slotUp;
+	PuzzleController.PuzzleColor slotLeft;
+	PuzzleController.PuzzleColor slotDown;
+	PuzzleController.PuzzleColor slotRight;
+	
+	public void TurnRight()
+	{
+		PuzzleController.PuzzleColor tempSlotUp = slotUp;
+		slotUp = slotLeft;
+		slotLeft = slotDown;
+		slotDown = slotRight;
+		slotRight = tempSlotUp;
+		CheckPuzzle ();
+	}
+	
+	public void TurnLeft()
+	{
+		PuzzleController.PuzzleColor tempSlotUp = slotUp;
+		slotUp = slotRight;
+		slotRight = slotDown;
+		slotDown = slotLeft;
+		slotLeft = tempSlotUp;
+		CheckPuzzle ();
+	}
+	
+	void CheckPuzzle()
+	{
+		//
+	}
+
+}
